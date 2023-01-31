2d44b1eac4ddc7747582627a5e3003c7c06d0d3b
hoaiphuongtvu <116790671+hoaiphuongtvu@users.noreply.github.com>
2023-01-31 10:10:58 +0700

    Add files via upload

---
+++ YeuCau6.cpp
@@ -0,0 +1,110 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+
+typedef struct
+{
+	char Ma[10];
+	char HoTen[40];
+}	SinhVien;
+
+void ReadFile(char *FileName);
+void WriteFile(char *FileName);
+void Search(char *FileName);
+
+int main()
+{
+	int c;
+	for(;;)
+	{
+		printf("\n \t*_*_*_*_*_*_*_*_*_*_*_*\n");
+		printf("\t 1. Nhap DSSV\n");
+		printf("\t 2. In DSSV\n");
+		printf("\t 3. Tim kiem\n");
+		printf("\t Ban chon 1,2,3,4: ");
+		scanf("%d",&c);
+		fflush(stdin);
+		if(c==1)
+		{
+			WriteFile("SinhVien.txt");
+		}
+		else if (c==2)
+		{
+			ReadFile("SinhVien.txt");
+		}
+		else if (c==3)
+		{
+			Search("SinhVien.txt");
+		}
+		else break;
+	}
+}
+
+void WriteFile(char *FileName)
+{
+	FILE *f;
+	int n,i;
+	SinhVien sv;
+	f=fopen(FileName,"ab");
+	printf("Nhap vao so luong sinh vien: ");
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{
+		printf("Sinh vien thu %i\n",i);
+		fflush(stdin);
+		printf(" - MSSV: ");
+		gets(sv.Ma);
+		printf(" - HoTen: ");
+		gets(sv.HoTen);
+		fwrite(&sv,sizeof(sv),1,f);
+	}
+	fclose(f);
+	printf("Bam phim bat ky de tiep tuc!!!");
+	getch();
+}
+
+void ReadFile(char *FileName)
+{
+	FILE *f;
+	SinhVien sv;
+	f=fopen(FileName,"rb");
+	printf(" MSSV | Ho va ten\n");
+	fread(&sv,sizeof(sv),1,f);
+	while (!feof(f))
+	{
+		printf(" %s | %s\n",sv.Ma,sv.HoTen);
+		fread(&sv,sizeof(sv),1,f);
+	}
+	fclose(f);
+	printf("Bam phim bat ky de tiep tuc!!!");
+	getch();
+}
+
+void Search(char *FileName)
+{ 
+	char MSSV[10];
+	FILE *f;
+	int Found=0;
+	SinhVien sv;
+	fflush(stdin);
+	printf("Ma so sinh vien can tim: ");
+	gets(MSSV);
+	f=fopen(FileName,"rb");
+	while (!feof(f) && Found==0)
+	{
+		fread(&sv,sizeof(sv),1,f);
+		if (strcmp(sv.Ma,MSSV)==0)
+			Found=1;
+	}
+	fclose(f);
+	if (Found == 1);
+	{
+		printf("Tim thay Sv co ma %s. Ho ten la: %s",sv.Ma,sv.HoTen);
+	}
+	else
+	{
+		printf("Khong tim thay sinh vien co ma %s",MSSV);
+	}
+	printf("\nBam phim bat ky de tiep tuc!!!");
+	getch();
+}
